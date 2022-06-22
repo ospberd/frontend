41ce59cc3b06b6a9e8f56bcaf85b57d108df0f1b
ospberd <ospberd@gmail.com>
2022-06-22 21:27:32 +0300

    bugfix

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,19 +1,14 @@
 <template>
   <div class="about">
     <v-row align="center" justify="center">
-    <v-card elevation="6" max-width="800px">
-        <v-card-title color="teal">
+      <v-card elevation="6" max-width="800px">
+        <v-card-title>
             {{$t("message")}}
         </v-card-title>
         <v-card-text>
           <h3> <span v-html='$t("myprog")'></span> </h3>
         </v-card-text> 
-
-    
-
-</v-card>
- 
-
+      </v-card>
     </v-row>  
   </div>
 </template>
